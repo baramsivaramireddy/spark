7b9cea88f961ada0515a6a9cfc2a5fb4b705c50c
baramsivaramireddy <baramsivarami@gmail.com>
2023-12-09 20:27:57 +0530

    setup next and sanity

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-5">
+     <p className="text-2xl font-extrabold text-red-500 ">Spark </p>
+    </main>
+  )
+}
